845b31a3c48e11e545900b578ce1444c8b6ac8c5
Mohammadreza <rezalahmi@gmail.com>
2020-07-19 07:51:44 +0430

    Update make_Social_network_watts_storage.R
    add comment to code

--- make_Social_network_watts_storage.R
+++ make_Social_network_watts_storage.R
@@ -3,12 +3,17 @@ make.random.social.network<-function(node){
   set.seed(1000*runif(1))
   net<-watts.strogatz.game(3,node,1,0.5)
   net<-simplify(net)
+  #we had two social institute in social network, Red and Blue
+  #by default all node are uninformed and we showed this with white color
   V(net)$color<-rep("white",node)
+  #the node select action in each time step based on its 
+  #action probability vector
   V(net)$action_prob_red<-0
   V(net)$action_prob_white<-1
   V(net)$action_prob_blue<-0
-  V(net)$profit<-0
+  #after adopt its action, node receives penalty from its environment
   V(net)$penalty<-0
+  #we had two type of node, seed and uninformed nodes
   V(net)$type<-0
   return(net)
 }#make social network
