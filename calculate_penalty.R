ea232b8f6d2d4656ec62424b9b3bcc438113fbca
Mohammadreza <rezalahmi@gmail.com>
2020-07-25 19:27:48 +0430

    update local rule function

--- calculate_penalty.R
+++ calculate_penalty.R
@@ -2,10 +2,14 @@ calculate_penalty<-function(net){
   avgDegree<-mean(degree(net))
   for(i in V(net)[type==0]){
     count_AgreeNode <- length(neighbors(net,i)[color==V(net)[i]$color])
-    if((count_AgreeNode + 1)/ length(neighbors(net,i))>= 1/avgDegree)
-      V(net)[i]$penalty<-0
-    else
-      V(net)[i]$penalty<-1
+    count_disAgreeNode <- length(neighbors(net,i)[color!=V(net)[i]$color])
+    if(V(net)[i]$color=="white" & count_disAgreeNode>=1)
+      V(net)[i]$penalty <- 1
+    else if (V(net)[i]$color=="white" & count_disAgreeNode<1)
+      V(net)[i]$penalty <- 0
+    else if (V(net)[i]$color!="white" & count_AgreeNode>=(1/avgDegree))
+      V(net)[i]$penalty <- 0
+    else V(net)[i]$penalty <- 1
   }
   return(net)
 }
