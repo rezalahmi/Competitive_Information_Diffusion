a40971c373a8327beceba6eb58c6068a9ffd531e
Mohammadreza <rezalahmi@gmail.com>
2020-07-26 17:19:36 +0430

    complete local rule of model

---
+++ Model_1.R
@@ -0,0 +1,86 @@
+mean_probability<-function(chain){
+  for(i in V(chain)[type==0]){
+    V(chain)[i]$mean_probability_red<-mean(neighbors(chain,i)$action_prob_red)
+    V(chain)[i]$mean_probability_blue<-mean(neighbors(chain,i)$action_prob_blue)
+    V(chain)[i]$mean_probability_white<-mean(neighbors(chain,i)$action_prob_white)
+  }
+  return(chain)
+}
+##############################################
+mean_penalty<-function(chain){
+  avgDegree<-mean(degree(chain))
+  for(i in V(chain)[type==0]){
+    Nl<-length(neighbors(chain,i)[color!="white"])
+    N<-length(neighbors(chain,i))
+      for(x in 0:floor(Nl/2)){
+       V(chain)[i]$mean_penalty_red<-V(chain)[i]$mean_penalty_red+choose(N,x)*(1-V(chain)[i]$mean_probability_red)^x*V(chain)[i]$mean_probability_red^(N-x)
+       V(chain)[i]$mean_penalty_blue<-V(chain)[i]$mean_penalty_blue+choose(N,x)*(1-V(chain)[i]$mean_probability_blue)^x*V(chain)[i]$mean_probability_blue^(N-x)
+       V(chain)[i]$mean_penalty_white<-V(chain)[i]$mean_penalty_white+choose(N,x)*(1-V(chain)[i]$mean_probability_white)^x*V(chain)[i]$mean_probability_white^(N-x)
+      }
+  }
+  return(chain)
+}
+################################################
+markov_chain<-function(chain){
+  for(i in V(chain)[type==0]){
+    c1<-V(chain)[i]$mean_penalty_red
+    c2<-V(chain)[i]$mean_penalty_blue
+    c3<-V(chain)[i]$mean_penalty_white
+    if(c1==0){V(chain)[i]$action_prob_red<-1}
+    else{V(chain)[i]$action_prob_red<-(1/c1)/(1/c1+1/c2+1/c3)}
+    if(c2==0){V(chain)[i]$action_prob_blue<-1}
+    else{V(chain)[i]$action_prob_blue<-(1/c2)/(1/c1+1/c2+1/c3)}
+    if(c3==0){V(chain)[i]$action_prob_white<-1}
+    else{V(chain)[i]$action_prob_white<-(1/c3)/(1/c1+1/c2+1/c3)}
+  }
+  
+  return(chain)
+}
+#################################################
+choose_color<-function(chain){
+  for(i in V(chain)[type==0]){
+    random_number<-sample(10000,1)/10000
+    p1<-V(chain)[i]$action_prob_red
+    p2<-V(chain)[i]$action_prob_blue
+    if(is.nan(p1))p1<-0
+    if(is.nan(p2))p2<-0
+    if(random_number <= p1){
+      V(chain)[i]$color<-"red"
+    }
+    else if((random_number>p1) & (random_number<=p2+p1)){
+      V(chain)[i]$color<-"blue"
+    }
+    else
+      V(chain)[i]$color<-"white"
+  }
+  return(chain)
+}
+#################################################
+report_markov_chain<-function(chain){
+  report<-data.frame(
+    nodeId = 1:vcount(chain),
+    action_prob_red=V(chain)$action_prob_red,
+    action_prob_blue=V(chain)$action_prob_blue,
+    action_prob_white=V(chain)$action_prob_white,
+    mean_probability_red=V(chain)$mean_probability_red,
+    mean_probability_blue=V(chain)$mean_probability_blue,
+    mean_probability_white=V(chain)$mean_probability_white,
+    mean_penalty_red=V(chain)$mean_penalty_red,
+    mean_penalty_blue=V(chain)$mean_penalty_blue,
+    mean_penalty_white=V(chain)$mean_penalty_white
+  )
+  write.csv(report,"C:\\report\\model_report.csv",row.names = FALSE)
+}
+##################################################
+lunch_model<-function(chain,number_of_repeat){
+  for(i in 1:number_of_repeat){
+    chain<-mean_probability(chain)
+    chain<-mean_penalty(chain)
+    chain<-markov_chain(chain)
+    chain<-choose_color(chain)
+    chain<-rewiring(chain)
+  }
+  report_markov_chain(chain)
+}
+##########################################
+
--- calculate_penalty.R
+++ calculate_penalty.R
@@ -1,15 +1,19 @@
 calculate_penalty<-function(net){
   avgDegree<-mean(degree(net))
-  for(i in V(net)[type==0]){
-    count_AgreeNode <- length(neighbors(net,i)[color==V(net)[i]$color])
-    count_disAgreeNode <- length(neighbors(net,i)[color!=V(net)[i]$color])
-    if(V(net)[i]$color=="white" & count_disAgreeNode>=1)
-      V(net)[i]$penalty <- 1
-    else if (V(net)[i]$color=="white" & count_disAgreeNode<1)
-      V(net)[i]$penalty <- 0
-    else if (V(net)[i]$color!="white" & count_AgreeNode>=(1/avgDegree))
-      V(net)[i]$penalty <- 0
-    else V(net)[i]$penalty <- 1
+  for(i in V(net)[type==0 & color=="white"]){
+    count_disagree<-length(neighbors(net,1)[color!="white"])
+    if(count_disagree>=1)
+      V(net)[i]$penalty<-1
+    else
+      V(net)[i]$penalty<-0
+  }
+  for(i in V(net)[type==0 & color!="white"]){
+    count_agree<-length(neighbors(net,i)[color==V(net)[i]$color])
+    count_dissagree<-length(neighbors(net,i)[color!="white" & color!=V(net)[i]$color])
+    if(count_dissagree/(count_agree+count_dissagree)>0.5)
+      V(net)[i]$penalty<-1
+    else
+      V(net)[i]$penalty<-0
   }
   return(net)
 }
